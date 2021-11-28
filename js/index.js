356d066ff402a8b43b429ada4e0f1289376ecd65
Mkkasim <94383661+Mkkasim@users.noreply.github.com>
2021-11-28 15:28:13 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const humburger = document.querySelector(".humburger");
+const navList = document.querySelector(".nav-list");
+
+if(humburger){
+    humburger.addEventListener("click", () => {
+        navList.classList.toggle("open");
+    });
+}
